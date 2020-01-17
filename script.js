8bd357d23c1e9a7e42c045f29485484585a73977
Alexandru Enache <44054368+steinhardt21@users.noreply.github.com>
2020-01-17 16:57:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,76 +1,277 @@
-
-
- // $(document).on('click','.first',function () {
-        
-			
-		
-
- // })
-
-/*test funzionamento*/
-console.log("Ciao 1!");
-// console.log(div.classList.contains("foo"));
-
-// $(".first").on("click", function() {
-		
-	// if($('div').hasClass('onglet_active')) 
-	// {
-		// $(".first").addClass("onglet_inactive");
-		// $(".onglet_active").removeClass("onglet_active");
-	// }
-// })
-
-console.log("Ciao 2!");
-
 /*************************GESTIONE CATEGORIE*********************************************/
 
-$(".first").click(function(){
-		$(".first").addClass("hello");
-});
-
-
-$(".onglet_inactive").click(function(){
-		$(".onglet_active").addClass("onglet_inactive");
-		$(".onglet_active").removeClass("onglet_active");
-		// $(this).addClass("hello");
-		$(this).removeClass("onglet_inactive");
-		$(this).addClass("onglet_active");
-		// $(this).addClass("false");
-		// console.log("Prova 2");
-});		
-	
-	
-	
-// function swapDiv(event,elem)
-// {
-	// elem.parentNode.insertBefore(elem,elem.parentNode.firstChild);
-// }	
-	
-	
-	
-	
-		// <!-- function swapDiv(event,elem){ -->
-			// <!-- elem.parentNode.insertBefore(elem,elem.parentNode.firstChild); -->
-		// <!-- } -->
-
-		// <!-- </script> -->
-
-
-		 // <!-- <div id="container"> -->
-				// <!-- <div onclick="swapDiv(event,this);">1</div> -->
-				// <!-- <div onclick="swapDiv(event,this);">2</div> -->
-				// <!-- <div onclick="swapDiv(event,this);">3</div> -->
-		 // <!-- </div> -->
-	
-
 // $(".first").click(function(){
-		// $(".first_category").css("display","block");
-		// $(".second_category").css("display","none");
-		// $(".third_category").css("display","none");
-		// $(".forth_category").css("display","none");
-		// $(".fifth_category").css("display","none");
-		// $(".sixth_category").css("display","none");
-	// });	
+		// $(".first").addClass("hello");
+// });
+
+
+// $(".onglet_inactive").click(function(){
+		// $(".onglet_active").addClass("onglet_inactive");
+		// $(".onglet_active").removeClass("onglet_active");
+		// // $(this).addClass("hello");
+		// $(this).removeClass("onglet_inactive");
+		// $(this).addClass("onglet_active");
+		// // $(this).addClass("false");
+		// // console.log("Prova 2");
+// });		
+	
+	
+var background = document.getElementById("white_back");
+
+$(".first").click(function(){
+	
+	   var element = document.getElementById("page_1");
+		
+	   if(element.classList.contains("onglet_active"))
+	   {
+			element.classList.remove("onglet_active");
+			element.classList.add("onglet_inactive");
+			
+			background.classList.remove("white_background_top");
+			background.classList.add("white_background_down");			
+			
+			$(".first_category").css("display","none");
+			$(".second_category").css("display","none");
+			$(".third_category").css("display","none");
+			$(".forth_category").css("display","none");
+			$(".fifth_category").css("display","none");
+			$(".sixth_category").css("display","none");
+
+			
+			// document.getElementById('page_1_content').hide(10);
+	   }
+	   else
+	   {
+			$(".first_category").css("display","block");
+			$(".second_category").css("display","none");
+			$(".third_category").css("display","none");
+			$(".forth_category").css("display","none");
+			$(".fifth_category").css("display","none");
+			$(".sixth_category").css("display","none");
+		   
+		   
+			$(".onglet").addClass("onglet_inactive");
+			$(".onglet").removeClass("onglet_active");
+		   
+			element.classList.remove("onglet_inactive");
+			element.classList.add("onglet_active");
+			
+			background.classList.remove("white_background_down");
+			background.classList.add("white_background_top");
+			
+			// document.getElementsByClassName('first_category')[0].style.visibility = 'visible';
+	   }
+});	
+
+$(".second").click(function(){
+	
+	   var element = document.getElementById("page_2");
+		   
+	   if(element.classList.contains("onglet_active"))
+	   {
+			element.classList.remove("onglet_active");
+			element.classList.add("onglet_inactive");
+			
+			background.classList.remove("white_background_top");
+			background.classList.add("white_background_down");		
+
+			$(".first_category").css("display","none");
+			$(".second_category").css("display","none");
+			$(".third_category").css("display","none");
+			$(".forth_category").css("display","none");
+			$(".fifth_category").css("display","none");
+			$(".sixth_category").css("display","none");			
+			
+	   }
+	   else
+	   {
+			$(".first_category").css("display","none");
+			$(".second_category").css("display","block");
+			$(".third_category").css("display","none");
+			$(".forth_category").css("display","none");
+			$(".fifth_category").css("display","none");
+			$(".sixth_category").css("display","none");
+		   
+		   
+			$(".onglet").addClass("onglet_inactive");
+			$(".onglet").removeClass("onglet_active");
+		   
+			element.classList.remove("onglet_inactive");
+			element.classList.add("onglet_active");
+			
+			background.classList.remove("white_background_down");
+			background.classList.add("white_background_top");
+	   }
+});	
+
+$(".third").click(function(){
+	
+	   var element = document.getElementById("page_3");
+		   
+	   if(element.classList.contains("onglet_active"))
+	   {
+			element.classList.remove("onglet_active");
+			element.classList.add("onglet_inactive");
+			
+			background.classList.remove("white_background_top");
+			background.classList.add("white_background_down");	
+
+			$(".first_category").css("display","none");
+			$(".second_category").css("display","none");
+			$(".third_category").css("display","none");
+			$(".forth_category").css("display","none");
+			$(".fifth_category").css("display","none");
+			$(".sixth_category").css("display","none");
+			
+	   }
+	   else
+	   {
+			$(".first_category").css("display","none");
+			$(".second_category").css("display","none");
+			$(".third_category").css("display","block");
+			$(".forth_category").css("display","none");
+			$(".fifth_category").css("display","none");
+			$(".sixth_category").css("display","none");
+			
+			
+			$(".onglet").addClass("onglet_inactive");
+			$(".onglet").removeClass("onglet_active");
+		   
+			element.classList.remove("onglet_inactive");
+			element.classList.add("onglet_active");
+			
+			background.classList.remove("white_background_down");
+			background.classList.add("white_background_top");
+	   }
+});	
+
+$(".fourth").click(function(){
+	
+	   var element = document.getElementById("page_4");
+		   
+	   if(element.classList.contains("onglet_active"))
+	   {
+			element.classList.remove("onglet_active");
+			element.classList.add("onglet_inactive");
+			
+			background.classList.remove("white_background_top");
+			background.classList.add("white_background_down");	
+
+			$(".first_category").css("display","none");
+			$(".second_category").css("display","none");
+			$(".third_category").css("display","none");
+			$(".forth_category").css("display","none");
+			$(".fifth_category").css("display","none");
+			$(".sixth_category").css("display","none");
+			
+	   }
+	   else
+	   {
+			$(".first_category").css("display","none");
+			$(".second_category").css("display","none");
+			$(".third_category").css("display","none");
+			$(".forth_category").css("display","block");
+			$(".fifth_category").css("display","none");
+			$(".sixth_category").css("display","none");
+		   
+			$(".onglet").addClass("onglet_inactive");
+			$(".onglet").removeClass("onglet_active");
+		   
+			element.classList.remove("onglet_inactive");
+			element.classList.add("onglet_active");
+			
+			background.classList.remove("white_background_down");
+			background.classList.add("white_background_top");
+	   }
+});	
+
+$(".fifth").click(function(){
+	
+	   var element = document.getElementById("page_5");
+		   
+	   if(element.classList.contains("onglet_active"))
+	   {
+			element.classList.remove("onglet_active");
+			element.classList.add("onglet_inactive");
+			
+			background.classList.remove("white_background_top");
+			background.classList.add("white_background_down");			
+			
+			$(".first_category").css("display","none");
+			$(".second_category").css("display","none");
+			$(".third_category").css("display","none");
+			$(".forth_category").css("display","none");
+			$(".fifth_category").css("display","none");
+			$(".sixth_category").css("display","none");
+	   }
+	   else
+	   {		   
+			$(".first_category").css("display","none");
+			$(".second_category").css("display","none");
+			$(".third_category").css("display","none");
+			$(".forth_category").css("display","none");
+			$(".fifth_category").css("display","block");
+			$(".sixth_category").css("display","none");
+		   
+			$(".onglet").addClass("onglet_inactive");
+			$(".onglet").removeClass("onglet_active");
+		   
+			element.classList.remove("onglet_inactive");
+			element.classList.add("onglet_active");
+			
+			background.classList.remove("white_background_down");
+			background.classList.add("white_background_top");
+	   }
+});	
+
+$(".sixth").click(function(){
+	
+	   var element = document.getElementById("page_6");
+		   
+	   if(element.classList.contains("onglet_active"))
+	   {
+			element.classList.remove("onglet_active");
+			element.classList.add("onglet_inactive");
+			
+			background.classList.remove("white_background_top");
+			background.classList.add("white_background_down");			
+			
+			$(".first_category").css("display","none");
+			$(".second_category").css("display","none");
+			$(".third_category").css("display","none");
+			$(".forth_category").css("display","none");
+			$(".fifth_category").css("display","none");
+			$(".sixth_category").css("display","none");
+	   }
+	   else
+	   {
+			$(".first_category").css("display","none");
+			$(".second_category").css("display","none");
+			$(".third_category").css("display","none");
+			$(".forth_category").css("display","none");
+			$(".fifth_category").css("display","none");
+			$(".sixth_category").css("display","block");
+			
+			$(".onglet").addClass("onglet_inactive");
+			$(".onglet").removeClass("onglet_active");
+		   
+			element.classList.remove("onglet_inactive");
+			element.classList.add("onglet_active");
+			
+			background.classList.remove("white_background_down");
+			background.classList.add("white_background_top");
+	   }
+});	
+
+
+
+
+
+
+
+$(".first").click(function(){
+		
+	});	
 
 // $(".second").click(function(){
 		// $(".first_category").css("display","none");
